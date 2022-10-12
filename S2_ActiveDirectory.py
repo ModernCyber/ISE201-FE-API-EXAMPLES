73b15b00b295adea3d9b54ccb7231eba4b5a35bc
ModernCyber <support@moderncyber.com>
2022-10-12 14:58:30 -0400

    added curl dep

--- S2_ActiveDirectory.py
+++ S2_ActiveDirectory.py
@@ -7,7 +7,7 @@ print(f"###############################################################")
 print("ISE 201 Lab Scenario 2 Exercise 3 - Using APIs to Join a PSN to the Active Directory Domain\n\n")
 
 # Variables
-baseurl = "https://admin:C1sco12345@ise-pan.dcloud.local:9060/ers/config/activedirectory"
+baseurl = "https://admin:C1sco12345@198.19.10.27:9060/ers/config/activedirectory"
 payload={}
 headers = {
   'Content-Type': 'application/json',
@@ -20,15 +20,24 @@ print(f"###############################################################")
 print("STEP 1: Determine Join Point ID of ABC-AD")
 url = f"{baseurl}/name/ABC-AD"
 response = requests.request("GET", url, headers=headers, data=payload, verify=False)
-adJP = json.loads(response.text)                    # Load the response text
-adJP = adJP["ERSActiveDirectory"]                   # Parse the results
-print(json.dumps(adJP, indent=4, sort_keys=True))
+
+if response.status_code == 200:                         # Make sure we got back a 200 OK
+    adJP = json.loads(response.text)                    # Load the response text
+    adJP = adJP["ERSActiveDirectory"]                   # Parse the results
+    print(json.dumps(adJP, indent=4, sort_keys=True))
+else:
+    print(f"Something went wrong!! Status Code: {response.status_code}") # Basic Error Handling
+    print(response.text)
+    quit()
+
 
 # Step 2
 print(f"###############################################################")
 print("STEP 2: Parse JSON Output for ID of ABC-AD\n\n")
+#adID = "/80a11f80-bc3e-11ec-9aeb-f65c9bad6277"
 adID = adJP['id']
 print(f"We found ABC-AD id: {adID}\n")
+
 
 # Step 3
 print(f"###############################################################")
